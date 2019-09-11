d63f5adcc4d6d30655a0e3e88df67463594b5b0f
Fernando Vicencio <fer.vicencio@gmail.com>
2019-09-11 11:01:47 -0300

    cors ok

---
+++ app/Http/Middleware/CORS.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $domains = ['http://localhost:3000','https://tudominio.cl'];
+        if (isset($request->server()['HTTP_ORIGIN'])) {
+            $origin = $request->server()['HTTP_ORIGIN'];
+            if (in_array($origin, $domains)) {
+                header('Access-Control-Allow-Origin: ' . $origin);
+                header('Access-Control-Expose-Headers: Access-Token, Uid, filename');
+                header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization, X-Requested-With');
+            }
+        }
+        return $next($request);
+    }
+}
